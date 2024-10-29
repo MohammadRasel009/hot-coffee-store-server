d5acbe41982e7ccde3f326f2f35ba4cda56ce0c7
unknown <MohammadRasel>
2024-10-29 20:52:22 +0600

    Added the final task

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -39,10 +39,45 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/coffee/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    })
+
     app.post('/coffee', async(req, res)=>{
       const newCoffee = req.body;
       console.log(newCoffee);
       const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
+    app.put('/coffee/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert: true};
+      const updatedCoffee = req.body;
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          photo: updatedCoffee.photo,
+        }
+      }
+
+      const result = await coffeeCollection.updateOne(filter, coffee, options)
+      res.send(result);
+
+    })
+
+    app.delete('/coffee/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await coffeeCollection.deleteOne(query);
       res.send(result);
     })
 
